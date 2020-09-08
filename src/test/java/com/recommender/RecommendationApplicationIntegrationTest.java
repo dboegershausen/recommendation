6ad0b8f4157511cd3bb2674a1ddb6e95bc312563
Diogo Boegershausen <diogo.boegershausen@fluig.com>
2020-09-08 08:48:13 -0300

    heroku deploy

---
+++ src/test/java/com/recommender/RecommendationApplicationIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.recommender;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RecommendationApplicationIntegrationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
